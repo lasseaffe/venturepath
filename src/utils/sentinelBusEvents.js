2d4926ba02192163246e087819e86aec34d6118f
lasseaffe <lasse.kusch@gmail.com>
2026-05-10 04:07:20 +0200

    feat(sentinel): add four new event type constants

---
+++ src/utils/sentinelBusEvents.js
@@ -0,0 +1,5 @@
+// src/utils/sentinelBusEvents.js
+export const VAULT_DOCUMENT_ADDED   = 'VAULT_DOCUMENT_ADDED';
+export const CANCELLATION_SIMULATED = 'CANCELLATION_SIMULATED';
+export const BREADCRUMB_UPDATED     = 'BREADCRUMB_UPDATED';
+export const PHOTO_ACTIVE           = 'PHOTO_ACTIVE';
