935a938a1caf0c35089965ea1a2d177fd9437c81
lasseaffe <lasse.kusch@gmail.com>
2026-05-12 12:30:19 +0200

    feat(homebase): add HOMEBASE_STOP_ADDED bus event

--- src/utils/sentinelBusEvents.js
+++ src/utils/sentinelBusEvents.js
@@ -6,3 +6,4 @@ export const PHOTO_ACTIVE           = 'PHOTO_ACTIVE';
 export const LAYER_TOGGLED          = 'LAYER_TOGGLED';
 export const TICKET_ADDED           = 'TICKET_ADDED';
 export const TICKET_SHARED          = 'TICKET_SHARED';
+export const HOMEBASE_STOP_ADDED    = 'HOMEBASE_STOP_ADDED';
